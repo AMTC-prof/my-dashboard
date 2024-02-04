16fa0736b82c3eb61dd6dd427eaf97704a2b8ea0
Alejandro Torres <amtc.prof@gmail.com>
2024-02-04 01:15:51 +0000

    Dashboard: Directiva @for y sus variables de control + directiva @empty

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -15,6 +15,8 @@ type Grade = 'A' | 'B' | 'F';
 export default class ControlFlowComponent { 
   public showContent = signal(false);
   public grade = signal<Grade>('A');
+  public frameworks = signal(['Angular', 'React', 'Vue', 'Svelte', 'Ember']);
+  public frameworks2 = signal([]);
 
   public toggleContent() {
     this.showContent.update( value => !value);
