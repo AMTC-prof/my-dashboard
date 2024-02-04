6738a9db719878e49d52e5090520d805953a4515
Alejandro Torres <amtc.prof@gmail.com>
2024-02-04 00:21:16 +0000

    Dashboard: Añadido componente dashboard de Tailwind y configuración del SideMenu

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SidemenuComponent } from '../shared/sidemenu/sidemenu/sidemenu.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    SidemenuComponent,
   ],
   templateUrl: './dashboard.component.html',
   styles: ``,
--- src/app/shared/sidemenu/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu/sidemenu.component.ts
@@ -1,14 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule
   ],
-  template: `<p>sidemenu works!</p>`,
+  templateUrl: 'sidemenu.component.html',
   styleUrl: './sidemenu.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidemenuComponent { }
+export class SidemenuComponent { 
+
+  /**
+   * *Configuracion de los elementos del menu a partir de las rutas existente
+   * *Descartamos la ruta por defecto del dashboard y la ruta del user/:id
+   */
+  public menuItems = routes.map(route => route.children ?? []).flat().
+    filter(route => route && route.path).
+    filter(route => !route.path?.includes(':'));
+  
+
+  constructor() {
+    
+    
+    
+   }
+
+
+}
